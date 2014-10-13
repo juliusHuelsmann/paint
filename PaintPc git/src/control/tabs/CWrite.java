e10ac5a4d25f2f6659cbd1b3329597f245c05a5a
Julius Huelsmann <julius_huelsmann@freenet.de>
2014-10-13 12:56:30 +0200

    CWrite

--- PaintPc git/src/control/tabs/CWrite.java
+++ PaintPc git/src/control/tabs/CWrite.java
@@ -28,8 +28,10 @@ public final class CWrite implements ActionListener {
      */
     private CWrite() { }
 
-    
-    private final static Color 
+    /**
+     * 
+     */
+    private static final Color 
     clr_green = new Color(2 * 2 * 2 * 2 * 2 * 2, 
             (int) (Math.pow(2, 2 + 2 + 2 + 1) 
                     + Math.pow(2, 2 + 2 + 2) + 2 + 2 + 1), 
@@ -40,17 +42,17 @@ public final class CWrite implements ActionListener {
             clr_darkBlue = new Color(112, 146, 190);
     
     /**
-     * 
+     * The pens.
      */
-    public final static Pen 
-    penWrite1 = new Pencil(Constants.PEN_ID_LINES, 2, Color.gray),
-    penWrite2 = new BallPen(Constants.PEN_ID_LINES, 2, clr_green),
-    penProof1 = new Pencil(Constants.PEN_ID_LINES, 2, clr_blue), 
-    penProof2 = new BallPen(Constants.PEN_ID_LINES, 2, Color.gray), 
-    penExmpl1 = new Pencil(Constants.PEN_ID_LINES, 2, clr_pink), 
-    penExmpl2 = new BallPen(Constants.PEN_ID_LINES, 2, clr_darkBlue),
-    penCmmnt1 = new Pencil(Constants.PEN_ID_LINES, 2, Color.gray),
-    penCmmnt2 = new BallPen(Constants.PEN_ID_LINES, 2, clr_pink);
+    public static final Pen 
+    PEN_THEOREM_1 = new Pencil(Constants.PEN_ID_LINES, 2, Color.gray),
+    PEN_THEOREM_2 = new BallPen(Constants.PEN_ID_LINES, 2, clr_green),
+    PEN_PROOF_1 = new Pencil(Constants.PEN_ID_LINES, 2, clr_blue), 
+    PEN_PROOF_2 = new BallPen(Constants.PEN_ID_LINES, 2, Color.gray), 
+    PEN_EXMPL_1 = new Pencil(Constants.PEN_ID_LINES, 2, clr_pink), 
+    PEN_EXMPL_2 = new BallPen(Constants.PEN_ID_LINES, 2, clr_darkBlue),
+    PEN_CMMNT_1 = new Pencil(Constants.PEN_ID_LINES, 2, Color.gray),
+    PEN_CMMNT_2 = new BallPen(Constants.PEN_ID_LINES, 2, clr_pink);
     
     
     
@@ -66,34 +68,34 @@ public final class CWrite implements ActionListener {
                 .getTb_beispiel().getActionCause())) {
             deactivate();
             Paint.getInstance().getJbtn_color1().setBackground(
-                    penExmpl1.getClr_foreground());
+                    PEN_EXMPL_1.getClr_foreground());
             Paint.getInstance().getJbtn_color2().setBackground(
-                    penExmpl2.getClr_foreground());
-            Status.setPenSelected1(Pen.clonePen(penExmpl1));
-            Status.setPenSelected2(Pen.clonePen(penExmpl2));
+                    PEN_EXMPL_2.getClr_foreground());
+            Status.setPenSelected1(Pen.clonePen(PEN_EXMPL_1));
+            Status.setPenSelected2(Pen.clonePen(PEN_EXMPL_2));
             
-            System.out.println(penExmpl1.getClr_foreground());
+            System.out.println(PEN_EXMPL_1.getClr_foreground());
             System.out.println(Status.getPenSelected1().getClr_foreground());
 
         } else if (_event.getSource().equals(Write.getInstance()
                 .getTb_bemerkung().getActionCause())) {
             deactivate();
             Paint.getInstance().getJbtn_color1().setBackground(
-                    penCmmnt1.getClr_foreground());
+                    PEN_CMMNT_1.getClr_foreground());
             Paint.getInstance().getJbtn_color2().setBackground(
-                    penCmmnt2.getClr_foreground());
-            Status.setPenSelected1(Pen.clonePen(penCmmnt1));
-            Status.setPenSelected2(Pen.clonePen(penCmmnt2));
+                    PEN_CMMNT_2.getClr_foreground());
+            Status.setPenSelected1(Pen.clonePen(PEN_CMMNT_1));
+            Status.setPenSelected2(Pen.clonePen(PEN_CMMNT_2));
 
         } else if (_event.getSource().equals(Write.getInstance()
                 .getTb_beweis().getActionCause())) {
             deactivate();
             Paint.getInstance().getJbtn_color1().setBackground(
-                    penProof1.getClr_foreground());
+                    PEN_PROOF_1.getClr_foreground());
             Paint.getInstance().getJbtn_color2().setBackground(
-                    penProof2.getClr_foreground());
-            Status.setPenSelected1(Pen.clonePen(penProof1));
-            Status.setPenSelected2(Pen.clonePen(penProof2));
+                    PEN_PROOF_2.getClr_foreground());
+            Status.setPenSelected1(Pen.clonePen(PEN_PROOF_1));
+            Status.setPenSelected2(Pen.clonePen(PEN_PROOF_2));
         } else if (_event.getSource().equals(Write.getInstance()
                 .getTb_headline1().getActionCause())) {
                 
@@ -107,11 +109,11 @@ public final class CWrite implements ActionListener {
                 .getTb_satz().getActionCause())) {
             deactivate();
             Paint.getInstance().getJbtn_color1().setBackground(
-                    penWrite1.getClr_foreground());
+                    PEN_THEOREM_1.getClr_foreground());
             Paint.getInstance().getJbtn_color2().setBackground(
-                    penWrite2.getClr_foreground());
-            Status.setPenSelected1(Pen.clonePen(penWrite1));
-            Status.setPenSelected2(Pen.clonePen(penWrite2));
+                    PEN_THEOREM_2.getClr_foreground());
+            Status.setPenSelected1(Pen.clonePen(PEN_THEOREM_1));
+            Status.setPenSelected2(Pen.clonePen(PEN_THEOREM_2));
             //TODO: update paint gui.
         }
 

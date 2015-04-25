6a86a38bc0dd460f01cceb9cbbcae9660132a40b
Julius Huelsmann <jhuelsmann@techfak.uni-bielefeld.de>
2015-04-25 20:16:18 +0200

    found out how to edit htmls

--- PaintNotes/src/main/java/test/TestWeb.java
+++ PaintNotes/src/main/java/test/TestWeb.java
@@ -10,6 +10,11 @@ import javax.swing.JFrame;
 import javax.swing.JScrollPane;
 import javax.swing.event.HyperlinkEvent;
 import javax.swing.event.HyperlinkListener;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.Element;
+import javax.swing.text.html.HTML;
+import javax.swing.text.html.HTMLDocument;
+import javax.swing.text.html.HTMLEditorKit;
 
 public class TestWeb {
 	
@@ -46,6 +51,8 @@ public class TestWeb {
 		try {
 			
 			myEditorPane.setPage(pageName);
+			System.out.println(myEditorPane.getActions());
+			
         } catch (IOException ex) {
             // handle error
             ex.printStackTrace();
@@ -60,6 +67,80 @@ public class TestWeb {
 		editorScrollPane.setMinimumSize(new Dimension(10, 10));
 		jf.add(editorScrollPane);
 		jf.repaint();
+		
+		
+		
+		
+		
+		
+		new Thread() {
+			public void run() {
+				
+				
+				//wait until loaded.
+				try {
+					Thread.sleep(2000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+				
+				 HTMLDocument doc = (HTMLDocument) myEditorPane.getDocument();
+			        HTMLEditorKit ekit = (HTMLEditorKit) myEditorPane.getEditorKit();
+			        int offset = myEditorPane.getSelectionStart();
+
+			        if (2.7 == 1) {
+
+				        buildTree("", doc.getRootElements()[0]);
+			            buildTree("", doc.getRootElements()[1]);
+			        }
+
+			        try {
+			            ekit.insertHTML(doc, offset, "<span>ahoj</span>", 0, 0, HTML.Tag.SPAN);
+			            Element ele = doc.getRootElements()[0];
+			            System.out.println();
+			            System.out.println(doc.getRootElements()[0].getElement(0).getElement(0).getElement(1));
+			            ele = ele.getElement(1);
+			            String ihtml = "<bar medium=\"#DEFAULT\" type=\"packaged\" source=\"identifier\" />";
+			            ihtml="<h1> asdf </h1>";
+			            String txt = 
+								"<label for=\"email\" content=\"testing@gmail.com\">Email <span class=\"required\">(required)</span> <span class=\"nopublish\">(Address never made public)</span></label>";
+			            doc.setInnerHTML(doc.getElement("email"), txt);
+//			            doc.setInnerHTML();
+			        }
+			        catch (BadLocationException ble) {
+			            throw new Error(ble);
+			        }
+			        catch (IOException ioe) {
+			            throw new Error(ioe);
+			        }
+			}
+		} .start();
+		
+		
+		
+		
+		
+		
+
+       
+	}
+	
+	private static void buildTree(String _t, Element ele) {
+		
+		
+		String parentName = "";
+		if (ele.getParentElement() != null) {
+
+			parentName = ele.getParentElement().getName();	
+		}
+		System.out.println(_t + ele.getName() + "from " + parentName);
+		for (int i = 0; i < ele.getElementCount(); i++) {
+			buildTree(_t + "\t", ele.getElement(i));
+		}
+		
+		
 	}
 
 }

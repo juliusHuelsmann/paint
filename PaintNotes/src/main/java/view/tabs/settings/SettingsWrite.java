c6da48961dc47488a2887f70946f0f3a47082e68
Julius Huelsmann <jhuelsmann@techfak.uni-bielefeld.de>
2015-08-13 00:50:25 +0200

    added javadoc

--- PaintNotes/src/main/java/view/tabs/settings/SettingsWrite.java
+++ PaintNotes/src/main/java/view/tabs/settings/SettingsWrite.java
@@ -3,6 +3,11 @@ package view.tabs.settings;
 @SuppressWarnings("serial")
 public class SettingsWrite extends Settings {
 
+	
+	
+	/**
+	 * Constructor for writing settings.
+	 */
 	public SettingsWrite() {
 		super("Write");
 	}

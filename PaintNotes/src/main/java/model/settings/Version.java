731f145dfba674edbb27ef59292037da9aeb08bd
Julius Huelsmann <jhuelsmann@techfak.uni-bielefeld.de>
2015-09-29 14:34:25 +0200

    release of new version. Scroll inncluded into MenuBar

--- PaintNotes/src/main/java/model/settings/Version.java
+++ PaintNotes/src/main/java/model/settings/Version.java
@@ -42,7 +42,7 @@ public final class Version {
 	 * This final static string contains the release-date of the current 
 	 * version.
 	 */
-	public static final String RELEASE_DATE = "2015 09 19";
+	public static final String RELEASE_DATE = "2015 09 29";
 	
 	/**
 	 * This final, static string contains information on the state of the 
@@ -68,7 +68,7 @@ public final class Version {
 	/**
 	 * Percentage of milestone done.
 	 */
-	public static final String PERCENTAGE_DONE = "24";
+	public static final String PERCENTAGE_DONE = "25";
 	
 	
 	/**
